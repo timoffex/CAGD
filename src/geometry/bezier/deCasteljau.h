db968603d0e5e6392c8ed712af3359e05266de94
timoffex <timoffex@gmail.com>
2018-03-31 18:00:29 -0700

    Improved subdivide() algorithm efficiency
    Created Geometry::Bezier::DeCasteljauScheme class to contiguously store
    all elements of a de Casteljau calculation.
    
    Now using DeCasteljauScheme class in the full subdivide() algorithm
    instead of a vector<vector<Point>>.

--- src/geometry/bezier/deCasteljau.h
+++ src/geometry/bezier/deCasteljau.h
@@ -8,6 +8,80 @@ namespace Geometry::Bezier
 {
 
   /**
+    @brief Stores all columns of a de Casteljau scheme contiguously.
+  */
+  template< typename Point >
+  class DeCasteljauScheme
+  {
+  public:
+    DeCasteljauScheme(std::vector<Point> initialPoints)
+      : mNumPoints(initialPoints.size())
+    {
+      mScheme.reserve((mNumPoints * (mNumPoints + 1)) / 2);
+      mScheme = initialPoints;
+    }
+
+    /**
+      @brief Accesses the last point in the scheme (the one that is in
+      a column of its own).
+    */
+    const Point &last() const
+    {
+      return mScheme[mScheme.size() - 1];
+    }
+
+    /**
+      @brief Accesses the `idx`th element in the `col`th column of the scheme.
+
+      Note: this point must either be one of the initial points (`col == 0`)
+      or it must have been push_back()-ed.
+
+      Columns are counted from the left, so that column 0 contains `numPoints`
+      elements. `idx` must be between 0 and `numPoints - col` (exclusive).
+    */
+    const Point &operator() (int col, int idx) const
+    {
+      // Index corresponding to `idx = 0` for this value of `col`.
+      // This is the result of summing (mNumPoints - k) for k = 0...col-1.
+      int idx0 = ((2 * mNumPoints - col + 1) * col) / 2;
+
+      return mScheme[idx0 + idx];
+    }
+
+    /**
+      @brief Accesses the `idx`th element in the `col`th column of the scheme.
+
+      Note: this point must either be one of the initial points (`col == 0`)
+      or it must have been push_back()-ed.
+
+      Columns are counted from the left, so that column 0 contains `numPoints`
+      elements. `idx` must be between 0 and `numPoints - col` (exclusive).
+    */
+    Point &operator() (int col, int idx)
+    {
+      // Index corresponding to `idx = 0` for this value of `col`.
+      // This is the result of summing (mNumPoints - k) for k = 0...col-1.
+      int idx0 = ((2 * mNumPoints - col + 1) * col) / 2;
+
+      return mScheme[idx0 + idx];
+    }
+
+    /**
+      @brief Initializes the next point in the scheme.
+
+      The "next" point is either the next point in the current column, or
+      the first point in the next column if the current column is finished.
+    */
+    void push_back(Point p)
+    {
+      mScheme.push_back(p);
+    }
+  private:
+    int mNumPoints;
+    std::vector<Point> mScheme;
+  };
+
+  /**
     @brief Performs the de Casteljau algorithm with parameter t. This is used
     to find a point on the Bezier curve given its control polygon.
 
@@ -114,50 +188,45 @@ namespace Geometry::Bezier
     std::vector<Point> newPoints;
     newPoints.reserve(numPoints);
 
-    std::vector<std::vector<Point>> iterations(numPoints, std::vector<Point>());
-
-    iterations[0] = points;
+    // This will store all columns of the de Casteljau scheme.
+    DeCasteljauScheme<Point> scheme(points);
 
     // Perform the iterations with t = t0, equivalent to the subdivide
     // algorithm with idx = 0.
     for (int iterationIdx = 1; iterationIdx < numPoints; ++iterationIdx)
     {
-      std::vector<Point> &prevIteration = iterations[iterationIdx - 1];
-      std::vector<Point> &iteration = iterations[iterationIdx];
-      iteration.reserve(numPoints - iterationIdx);
-
       for (int pointIdx = 0; pointIdx < numPoints - iterationIdx; ++pointIdx)
       {
-        const Point &p1 = prevIteration[pointIdx];
-        const Point &p2 = prevIteration[pointIdx + 1];
-        iteration.push_back(p1 + t0 * (p2 - p1));
+        const Point &p1 = scheme(iterationIdx - 1, pointIdx);
+        const Point &p2 = scheme(iterationIdx - 1, pointIdx + 1);
+
+        // The order of traversal is important here. Traverse the elements
+        // in a column from bottom to top, then traverse columns left-to-right.
+        scheme.push_back(p1 + t0 * (p2 - p1));
       }
     }
 
     // This is the first point. It is equal to the result of
     //   `subdivide(points, 0, t0, t1)`
-    newPoints.push_back(iterations[numPoints - 1][0]);
+    newPoints.push_back(scheme.last());
 
     // Now figure out the other points. Since point K only requires the last
-    // K scalars to be t1, we can reuse some the previous results.
+    // K scalars to be t1, we can reuse some of the previous results.
     for (int newPointIdx = 1; newPointIdx < numPoints; ++newPointIdx)
     {
       for (int iterationIdx = numPoints - newPointIdx; iterationIdx < numPoints; ++iterationIdx)
       {
-        std::vector<Point> &prevIteration = iterations[iterationIdx - 1];
-        std::vector<Point> &iteration = iterations[iterationIdx];
-
         for (int pointIdx = 0; pointIdx < numPoints - iterationIdx; ++pointIdx)
         {
-          const Point &p1 = prevIteration[pointIdx];
-          const Point &p2 = prevIteration[pointIdx + 1];
-          iteration[pointIdx] = p1 + t1 * (p2 - p1);
+          const Point &p1 = scheme(iterationIdx - 1, pointIdx);
+          const Point &p2 = scheme(iterationIdx - 1, pointIdx + 1);
+          scheme(iterationIdx, pointIdx) = p1 + t1 * (p2 - p1);
         }
       }
 
       // This is the same as the result of
       //   `subdivide(points, newPointIdx, t0, t1)`
-      newPoints.push_back(iterations[numPoints - 1][0]);
+      newPoints.push_back(scheme.last());
     }
 
     return newPoints;
